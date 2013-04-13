f29e1c8c721bb03ac1863865de51ea4abb5092b2
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2013-04-13 15:14:46 +0200

    A:Doc & formatting

--- src/btccharts/transaction.go
+++ src/btccharts/transaction.go
@@ -8,6 +8,7 @@ import "io"
 import "log"
 import "encoding/json"
 
+// Represents the a json messsage row from btccharts
 type Message struct {
 	Symbol, Volume, Id, Timestamp, Price string
 }
@@ -17,22 +18,25 @@ type Message struct {
 /*}*/
 
 /*const input = "{\"volume\": 4.0, \"timestamp\": 1365812301, \"price\": 114.0, \"symbol\": \"virtexCAD\", \"id\": 21913359}"*/
+
+// Parses a string from btccharts to json. We have to do some hacky reggex to
+// make all values strings (fuck float64) before parsing it with std json go lib
 func GetMessage(data string) Message {
-    var tmp  string
-    tmp = regexp.MustCompile(`: `).ReplaceAllString(data,": \"")
-    tmp = regexp.MustCompile(`[,]`).ReplaceAllString(tmp,"\",")
-    tmp = regexp.MustCompile(`[}]`).ReplaceAllString(tmp,"\"}")
-    tmp = regexp.MustCompile(`["]{2}`).ReplaceAllString(tmp,"\"")
-    log.Println(tmp)
-    dec := json.NewDecoder(strings.NewReader(tmp))
-    var m Message
-    for {
-        if err := dec.Decode(&m); err == io.EOF {
-            break
-        } else if err != nil {
-            log.Fatal(err)
-        }
-    }
+	var tmp string
+	tmp = regexp.MustCompile(`: `).ReplaceAllString(data, ": \"")
+	tmp = regexp.MustCompile(`[,]`).ReplaceAllString(tmp, "\",")
+	tmp = regexp.MustCompile(`[}]`).ReplaceAllString(tmp, "\"}")
+	tmp = regexp.MustCompile(`["]{2}`).ReplaceAllString(tmp, "\"")
+	log.Println(tmp)
+	dec := json.NewDecoder(strings.NewReader(tmp))
+	var m Message
+	for {
+		if err := dec.Decode(&m); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+	}
 
 	return m
 }
