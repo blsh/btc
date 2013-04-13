2abd308cbbce1f7195c69bdfd15aaca32f3872de
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2013-04-13 15:55:28 +0200

    Added Tests

--- src/btccharts/transaction_test.go
+++ src/btccharts/transaction_test.go
@@ -3,17 +3,17 @@ package btccharts
 import "testing"
 
 // Example json input row from btccharts
-const input = `{"volume": 4.0, "timestamp": 1365812301, "price": 114.0, "symbol": "virtexCAD", "id": 21913359}`
+const input = `{"volume": 4.0, "timestamp": 1365812301, "price": 114.0, "symbol": "mtgoxUSD", "id": 21913359}`
 
 func TestGetMessage(t *testing.T) {
 	m := GetMessage(input)
-	if m.Volume != "4.0" || m.Timestamp != "1365812301" || m.Price != "114.0" || m.Symbol != "virtexCAD" || m.Id != "21913359" {
+	if m.Volume != "4.0" || m.Timestamp != "1365812301" || m.Price != "114.0" || m.Symbol != "mtgoxUSD" || m.Id != "21913359" {
 		t.Errorf("Fail %#v", m)
 	}
 }
 
 func TestConvertAllJsonValuesToString(t *testing.T) {
-	const expected = `{"volume": "4.0", "timestamp": "1365812301", "price": "114.0", "symbol": "virtexCAD", "id": "21913359"}`
+	const expected = `{"volume": "4.0", "timestamp": "1365812301", "price": "114.0", "symbol": "mtgoxUSD", "id": "21913359"}`
 	result := ConvertAllJsonValuesToString(input)
 	checkStrings(expected, result, t)
 }
